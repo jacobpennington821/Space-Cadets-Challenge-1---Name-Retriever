2f28fc8b1aa655b5746e751b7090971e78b3c3e2
crackers821 <moofish821@gmail.com>
2017-10-05 20:44:20 +0100

    Added String Methods
    Completed extracting names from html

--- src/nameRetriever/Retriever.java
+++ src/nameRetriever/Retriever.java
@@ -1,8 +1,43 @@
 package nameRetriever;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Retriever {
+	
+	Scanner scanner = new Scanner(System.in);
 
 	public Retriever() {
-		
+		System.out.println("Enter an email id: ");
+		String input = scanner.nextLine();
+		String address = "http://www.ecs.soton.ac.uk/people/" + input;
+		try {
+			URL url = new URL(address);
+			
+			BufferedReader html = new BufferedReader(
+					new InputStreamReader(url.openStream()));
+			String htmlLine;
+			String searchText = "\"name\">";
+			while ((htmlLine = html.readLine()) != null) {
+				int nameIndex = htmlLine.indexOf(searchText);
+				if(nameIndex != -1){
+					String shortenedString = htmlLine.substring(nameIndex + searchText.length(), htmlLine.length());
+					shortenedString = shortenedString.substring(0, shortenedString.indexOf("</h1>"));
+					System.out.println(htmlLine);
+					System.out.println(shortenedString);
+				}
+			}
+			html.close();
+			System.out.println("No such id!");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
 }
